4d61ffca16e8f06cad57a60f3ebd23229e61c26d
Ubugeeei <ubuge1122@gamil.com>
2022-07-02 19:16:03 +0900

    feat: static graphql

--- Makefile
+++ Makefile
@@ -5,7 +5,4 @@ stop:
 	lsof -i:8080 -t | xargs kill
 
 test:
-	make run &
-	sleep 5s
-	curl http://127.0.0.1:8080/hello/Ubugeeei
-	make stop
+	curl -X POST -H "Content-Type: application/json" -d '{"query": "query { staticValue }"}' http://127.0.0.1:8080
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,44 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{guard, web, web::Data, App, HttpResponse, HttpServer, Result};
+use async_graphql::{
+    http::{playground_source, GraphQLPlaygroundConfig},
+    EmptyMutation, EmptySubscription, Object, Schema,
+};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+struct Query;
+#[Object]
+impl Query {
+    async fn static_value(&self) -> usize {
+        42
+    }
 }
 
-#[actix_web::main] // or #[tokio::main]
+type ApiSchema = Schema<Query, EmptyMutation, EmptySubscription>;
+
+async fn index(schema: web::Data<ApiSchema>, req: GraphQLRequest) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+async fn index_playground() -> Result<HttpResponse> {
+    let source = playground_source(GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"));
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(source))
+}
+
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(greet))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+
+    println!("listen ...");
+    println!("http://127.0.0.1:8080");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(schema.clone()))
+            .service(web::resource("/").guard(guard::Post()).to(index))
+            .service(web::resource("/").guard(guard::Get()).to(index_playground))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
